0c5e5c68e0a0dcbcc5becb11dfa3a23167c67853
ashutoshwahane <ashutosh.wahane10@gmail.com>
2021-07-14 01:08:44 +0530

    initial commit

---
+++ app/src/main/java/xyz/cybernerd404/cryptox/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.cybernerd404.cryptox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
