26592d47214cdcd65d3c90a9817d34215443008d
ashutoshwahane <ashutosh.wahane10@gmail.com>
2021-07-23 22:53:49 +0530

    Base Url and Retrofit Api instance

---
+++ app/src/main/java/xyz/cybernerd404/cryptox/network/RemoteDataSource.kt
@@ -0,0 +1,33 @@
+package xyz.cybernerd404.cryptox.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import xyz.cybernerd404.cryptox.BuildConfig
+
+class RemoteDataSource {
+
+    companion object {
+        private const val BASE_URL = "https://api.coingecko.com/api/v3/"
+    }
+
+    fun <Api> buildApi(
+        api: Class<Api>,
+    ): Api {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(
+                OkHttpClient.Builder().also { client ->
+                    if (BuildConfig.DEBUG) {
+                        val logging = HttpLoggingInterceptor()
+                        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+                        client.addInterceptor(logging)
+                    }
+                }.build()
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(api)
+    }
+}
