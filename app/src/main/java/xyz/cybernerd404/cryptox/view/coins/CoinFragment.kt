8fa3a93bb56fe83719a193209eacc6fda571c1b4
ashutoshwahane <ashutosh.wahane10@gmail.com>
2021-07-23 22:50:10 +0530

    fetched crypto currency list

--- app/src/main/java/xyz/cybernerd404/cryptox/view/coins/CoinFragment.kt
+++ app/src/main/java/xyz/cybernerd404/cryptox/view/coins/CoinFragment.kt
@@ -1,60 +1,64 @@
 package xyz.cybernerd404.cryptox.view.coins
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import xyz.cybernerd404.cryptox.R
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import xyz.cybernerd404.cryptox.adapter.HomeCoinAdapter
+import xyz.cybernerd404.cryptox.databinding.FragmentCoinBinding
+import xyz.cybernerd404.cryptox.network.CryptoApi
+import xyz.cybernerd404.cryptox.network.Resource
+import xyz.cybernerd404.cryptox.repository.CoinRepository
+import xyz.cybernerd404.cryptox.view.base.BaseFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class CoinFragment : BaseFragment<CoinViewModel, FragmentCoinBinding, CoinRepository>() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CoinFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CoinFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var coinAdapter :HomeCoinAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    override fun getViewModel() = CoinViewModel::class.java
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coin, container, false)
-    }
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentCoinBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CoinFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CoinFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun getFragmentRepository() = CoinRepository(remoteDataSource.buildApi(CryptoApi::class.java))
+
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        coinAdapter = HomeCoinAdapter(requireContext())
+        binding.homeCoinRv.adapter = coinAdapter
+        binding.homeCoinRv.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, true)
+
+        viewModel.getCoin()
+        binding.progessBarCoin.visibility = View.VISIBLE
+
+        viewModel.coinResponse.observe(viewLifecycleOwner, Observer {
+            when(it){
+                is Resource.Success -> {
+                    coinAdapter.setCoin(it.value)
+                    binding.progessBarCoin.visibility = View.GONE
+                }
+                is Resource.Failure -> {
+                    binding.progessBarCoin.visibility = View.GONE
+                    Toast.makeText(requireContext(), "Response Failure", Toast.LENGTH_SHORT).show()
                 }
             }
+        })
+
+
+
+
     }
+
+
+
+
 }
